0500260dffa1364d5482b6a2863670a9a2882d04
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-11-27 21:26:04 +0600

    api for buyer route added

--- index.js
+++ index.js
@@ -149,6 +149,14 @@ async function run() {
       const user = await usersCollection.findOne(query);
       res.send({ isSeller: user?.role === "Seller" });
     });
+
+    // api to check buyers to access my orders on dashboard
+    app.get("/users/buyers/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isBuyer: user?.role === "Buyer" });
+    });
   } finally {
   }
 }
